eece5f64d7fecd26dc92d7033de6d5ce1082d52c
OrlanDooos <roman.palei1@gmail.com>
2023-02-16 19:40:04 +0200

    timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
   testDir: './specs',
-  timeout: 30 * 1000,
+  timeout: 50 * 1000,
   expect: {
     timeout: 5000,
   },
