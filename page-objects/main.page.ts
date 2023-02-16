b0b9117fafee27349a2244da7c604bd7f4223110
OrlanDooos <roman.palei1@gmail.com>
2023-02-16 18:47:06 +0200

    test

--- page-objects/main.page.ts
+++ page-objects/main.page.ts
@@ -18,7 +18,7 @@ export class MainPage {
 
   async openMainPageUrl() {
     await this.page.goto('/', { waitUntil: 'load' });
-    await this.page.locator('//a[@href="https://rozetka.com.ua/ua/"]').click();
+   // await this.page.locator('//a[@href="https://rozetka.com.ua/ua/"]').click();
   }
 
   async searchItem(itemName: string) {
