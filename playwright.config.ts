200a863fc5dc0f2a49ee70f1bb7be61ed2961985
OrlanDooos <roman.palei1@gmail.com>
2023-09-15 21:09:05 +0300

    test 1

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,11 +15,11 @@ const config: PlaywrightTestConfig = {
     baseURL: 'https://rozetka.com.ua/ua/',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
-    headless: false,
+    headless: true,
     viewport: { width: 1440, height: 720 },
-    video: 'retain-on-failure',
-    userAgent:
-      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
+    video: 'retain-on-failure'
+    //userAgent:
+    //  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
   },
 };
 
